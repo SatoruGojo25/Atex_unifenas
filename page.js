571500cab47a4a6523366fed248064b105df779f
SatoruGojo25 <edukramil78@gmail.com>
2025-11-09 20:29:44 -0300

    Atualiação

--- page.js
+++ page.js
@@ -1,19 +1,19 @@
 const myModalEl = document.querySelector('#myModal')
-const modal = new bootstrap.Modal(myModalEl) // initialized with defaults
+const modal = new bootstrap.Modal(myModalEl)
 
 const configObject = { keyboard: false }
-const modal1 = new bootstrap.Modal(myModalEl, configObject) // initialized with no keyboard
+const modal1 = new bootstrap.Modal(myModalEl, configObject) 
 const myCollapseEl = document.querySelector('#myCollapse')
 
 myCollapseEl.addEventListener('shown.bs.collapse', event => {
-  // Action to execute once the collapsible area is expanded
+  
 })
 const myCarouselEl = document.querySelector('#myCarousel')
-const carousel = bootstrap.Carousel.getInstance(myCarouselEl) // Retrieve a Carousel instance
+const carousel = bootstrap.Carousel.getInstance(myCarouselEl) 
 
 myCarouselEl.addEventListener('slid.bs.carousel', event => {
-  carousel.to('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
+  carousel.to('2') 
 })
 
-carousel.to('1') // Will start sliding to the slide 1 and returns to the caller
-carousel.to('2') // !! Will be ignored, as the transition to the slide 1 is not finished !!
+carousel.to('1') 
+carousel.to('2') 
